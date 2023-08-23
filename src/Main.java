1d34d2244467077d67d6be68bf151a530cac1374
Joaquin <71148894+1Neikowo@users.noreply.github.com>
2023-08-22 23:12:22 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         agregarVinilos(vinilos, "Daft Punk", "Random Access Memories", "2013");
         agregarVinilos(vinilos, "The Strokes", "The New Abnormal", "323");
         agregarVinilos(vinilos, "The keku", "The New Abnormal", "323");
+        agregarVinilos(vinilos, "The Doors", "The New Abnormal", "323");
 
         System.out.println("-------------------------");
         System.out.println("Espacio máximo colección: "+vinilos.length);
